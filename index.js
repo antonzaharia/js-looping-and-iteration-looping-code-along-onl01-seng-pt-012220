b7eb507c64325c514603f879d62b1da50d3710a0
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-18 19:44:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function writeCards(giftsArray, eventName) {
-  for (let i = 0; , i < giftsArray.length; , i++){
-    gitsArray.splice(i, 1, `Thank you, ${giftsArray[i]}, for the wonderful ${eventName} gift!`);
+  for (let i = 0; i < giftsArray.length; i++){
+    giftsArray.splice(i, 1, `Thank you, ${giftsArray[i]}, for the wonderful ${eventName} gift!`);
   }
   return giftsArray;
 }
